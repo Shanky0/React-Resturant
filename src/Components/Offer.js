1462c6b31b556521b500432fdded6ab327f315d3
Shashank Rai <108118506+shanky-ced@users.noreply.github.com>
2022-07-31 15:10:13 +0530

    Update Offer.js

--- src/Components/Offer.js
+++ src/Components/Offer.js
@@ -8,51 +8,60 @@ const Offer = () => {
 
         <h1>OUR <span>Offers</span></h1>
         <div id="box-container">
-          <div class="box">
+          <div className="box">
             <img src="https://www.thespruceeats.com/thmb/yLyKgqIj6-SLt9gJkgkjfIZtfC0=/4300x2867/filters:no_upscale():max_bytes(150000):strip_icc()/chole-chickpea-curry-1957946-hero-01-cddf957cc78d4032ba29ec2adbc05a97.jpg" alt="" />
             <h3>White </h3>
-            <div class="price">₹450.00 <span>₹500.00</span></div>
+            <div className="price">₹450.00 <span>₹500.00</span></div>
+            <button className="btn"> Add to Cart</button>
           </div>
 
-          <div class="box">
+          <div className="box">
             <img src="https://www.thespruceeats.com/thmb/yLyKgqIj6-SLt9gJkgkjfIZtfC0=/4300x2867/filters:no_upscale():max_bytes(150000):strip_icc()/chole-chickpea-curry-1957946-hero-01-cddf957cc78d4032ba29ec2adbc05a97.jpg" alt="" />
             <h3>White </h3>
-            <div class="price">₹450.00 <span>₹500.00</span></div>
+            <div className="price">₹450.00 <span>₹500.00</span></div>
+            <button className="btn"> Add to Cart</button>
           </div>
-          <div class="box">
+          <div className="box">
             <img src="https://www.thespruceeats.com/thmb/yLyKgqIj6-SLt9gJkgkjfIZtfC0=/4300x2867/filters:no_upscale():max_bytes(150000):strip_icc()/chole-chickpea-curry-1957946-hero-01-cddf957cc78d4032ba29ec2adbc05a97.jpg" alt="" />
             <h3>White </h3>
-            <div class="price">₹450.00 <span>₹500.00</span></div>
+            <div className="price">₹450.00 <span>₹500.00</span></div>
+            <button className="btn"> Add to Cart</button>
           </div>
-          <div class="box">
+          <div className="box">
             <img src="https://www.thespruceeats.com/thmb/yLyKgqIj6-SLt9gJkgkjfIZtfC0=/4300x2867/filters:no_upscale():max_bytes(150000):strip_icc()/chole-chickpea-curry-1957946-hero-01-cddf957cc78d4032ba29ec2adbc05a97.jpg" alt="" />
             <h3>White </h3>
-            <div class="price">₹450.00 <span>₹500.00</span></div>
+            <div className="price">₹450.00 <span>₹500.00</span></div>
+            <button className="btn"> Add to Cart</button>
           </div>
-          <div class="box">
+          <div className="box">
             <img src="https://www.thespruceeats.com/thmb/yLyKgqIj6-SLt9gJkgkjfIZtfC0=/4300x2867/filters:no_upscale():max_bytes(150000):strip_icc()/chole-chickpea-curry-1957946-hero-01-cddf957cc78d4032ba29ec2adbc05a97.jpg" alt="" />
             <h3>White </h3>
-            <div class="price">₹450.00 <span>₹500.00</span></div>
+            <div className="price">₹450.00 <span>₹500.00</span></div>
+            <button className="btn"> Add to Cart</button>
           </div>
-          <div class="box">
+          <div className="box">
             <img src="https://www.thespruceeats.com/thmb/yLyKgqIj6-SLt9gJkgkjfIZtfC0=/4300x2867/filters:no_upscale():max_bytes(150000):strip_icc()/chole-chickpea-curry-1957946-hero-01-cddf957cc78d4032ba29ec2adbc05a97.jpg" alt="" />
             <h3>White </h3>
-            <div class="price">₹450.00 <span>₹500.00</span></div>
+            <div className="price">₹450.00 <span>₹500.00</span></div>
+            <button className="btn"> Add to Cart</button>
           </div>
-          <div class="box">
+          <div className="box">
             <img src="https://www.thespruceeats.com/thmb/yLyKgqIj6-SLt9gJkgkjfIZtfC0=/4300x2867/filters:no_upscale():max_bytes(150000):strip_icc()/chole-chickpea-curry-1957946-hero-01-cddf957cc78d4032ba29ec2adbc05a97.jpg" alt="" />
             <h3>White </h3>
-            <div class="price">₹450.00 <span>₹500.00</span></div>
+            <div className="price">₹450.00 <span>₹500.00</span></div>
+            <button className="btn"> Add to Cart</button>
           </div>
-          <div class="box">
+          <div className="box">
             <img src="https://www.thespruceeats.com/thmb/yLyKgqIj6-SLt9gJkgkjfIZtfC0=/4300x2867/filters:no_upscale():max_bytes(150000):strip_icc()/chole-chickpea-curry-1957946-hero-01-cddf957cc78d4032ba29ec2adbc05a97.jpg" alt="" />
             <h3>White </h3>
-            <div class="price">₹450.00 <span>₹500.00</span></div>
+            <div className="price">₹450.00 <span>₹500.00</span></div>
+            <button className="btn"> Add to Cart</button>
           </div>
-          <div class="box">
+          <div className="box">
             <img src="https://www.thespruceeats.com/thmb/yLyKgqIj6-SLt9gJkgkjfIZtfC0=/4300x2867/filters:no_upscale():max_bytes(150000):strip_icc()/chole-chickpea-curry-1957946-hero-01-cddf957cc78d4032ba29ec2adbc05a97.jpg" alt="" />
             <h3>White </h3>
-            <div class="price">₹450.00 <span>₹500.00</span></div>
+            <div className="price">₹450.00 <span>₹500.00</span></div>
+            <button className="btn"> Add to Cart</button>
           </div>
         </div>
       </section>
